a8fc1bfbdc2732ff451876da295fa4aab1404341
Abdurakhmanova Nodirabegim <abdurakhmanovanodirabegim@gmail.com>
2026-04-07 17:23:48 +0600

    removed comments

--- homework_1.py
+++ homework_1.py
@@ -4,9 +4,9 @@ def main_page(page: ft.Page):
     page.title = "counting numbers of press button"
     
     count = 0
-    text_pressed = ft.Text("Pressed: 0 times", size=20) # добавил размер текста для наглядности
+    text_pressed = ft.Text("Pressed: 0 times", size=20) 
 
-    def button_clicked(e): # добавил аргумент 'e', это стандарт для событий Flet
+    def button_clicked(e):
         nonlocal count
         count += 1
         text_pressed.value = f"Pressed: {count} times"
